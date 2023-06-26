29136e62d09dcb5e33f88af9907858fb53a43b78
Rifqi Finaldy <rifqifinaldy@gmail.com>
2023-06-26 13:25:57 +0700

    rebackup

--- src/pages/tentang-kami.tsx
+++ src/pages/tentang-kami.tsx
@@ -3,20 +3,12 @@ import * as React from "react";
 
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import HeroSecondary from "../section/hero/secondary";
-import Timeline from "../section/timeline";
-import OurMission from "../section/ourMission";
-import OurTeam from "../section/ourTeam";
 
 const AboutUs: React.FC<PageProps> = () => {
   return (
     <main>
       <Navbar />
-      <HeroSecondary />
-      <Timeline />
-      <OurMission />
-      <OurTeam />
-      <Footer withContactAdmin />
+      <Footer />
     </main>
   );
 };
