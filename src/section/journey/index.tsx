33591cb6a238ceac4a0a2ba1e13a28fa7773d1a3
Rifqi Finaldy <rifqifinaldy@gmail.com>
2023-06-26 15:11:14 +0700

    feat: added journey section timeline

--- src/section/journey/index.tsx
+++ src/section/journey/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Title from "../../components/title";
+import Timeline from "../../components/timeline/timeline";
+import { journeyData } from "../../dummy/journey";
 
 const JourneySection: React.FC = () => {
   return (
-    <section id="section-journey">
+    <section id="section-journey" className="px-10 md:px-36">
       <Title subtitle="A Story to get closer to Wool">Our Journey</Title>
-      <div></div>
+      <div className="py-10">
+        {journeyData.map((journey, i) => {
+          return (
+            <Timeline
+              key={journey.id}
+              time={journey.time}
+              illustration={journey.illustration}
+              description={journey.description}
+              isActive={i === 0}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 };
---
+++ src/section/ourMission/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurMissionSection = () => {
+  return (
+    <section id="section-mission">
+        
+    </section>
+  )
+}
+
+export default OurMissionSection
