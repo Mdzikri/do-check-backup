3c7e41de48f1775351e598162657ffe21c9014ca
Rifqi Finaldy <rifqifinaldy@gmail.com>
2023-06-27 13:11:49 +0700

    added copyright element on the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,67 +6,68 @@ import { contactData } from "../../dummy/contact";
 
 export default function Footer() {
   return (
-    <>
-      <footer className="bg-[#d4ecfd] relative mt-20 px-10 md:px-32 py-10 md:py-20">
-        <div className="md:flex">
-          <div className="md:w-[33%]">
-            <div className="w-[200px]">
-              <img src={WoolLogo} alt="We open our lives" placeholder="blur" />
-            </div>
-            <span className="block my-5 text-[#3e3e3e] font-bold text-[18px]">
-              Let's Find A Solution To Your Problem
-            </span>
-            <span className="block my-5 text-[#3e3e3e] text-[16px]">
-              {contactData.address}
-            </span>
+    <footer className="bg-[#d4ecfd] relative mt-20 px-10 md:px-32 pt-5 md:pt-20">
+      <div className="md:flex">
+        <div className="md:w-[33%]">
+          <div className="w-[200px]">
+            <img src={WoolLogo} alt="We open our lives" placeholder="blur" />
           </div>
-          <div className="md:w-[33%] md:pl-20">
-            <span className="font-bold">Navigation</span>
-            <ul className="md:pt-10 text-[#3e3e3e]">
-              <li className="my-2">
-                <a href="">About Us</a>
-              </li>
-              <li className="my-2">
-                <a href="">Wool For Company</a>
-              </li>
-              <li className="my-2">
-                <a href="">Join as a Coach</a>
-              </li>
-            </ul>
-          </div>
-          <div className="md:w-[33%] md:pl-20">
-            <span className="font-bold">Business & Cooperation</span>
-            <ul className="md:pt-10 text-[#3e3e3e] font-bold">
-              <li className="my-2">
-                <a
-                  target="_blank"
-                  href={`https://wa.me/${process.env.WA_PHONE_NUMBER}`}
-                  className="flex items-center"
-                >
-                  <img
-                    className="w-[20px] mr-2"
-                    src={PhoneIcon}
-                    alt="Phone Icon"
-                    placeholder="blur"
-                  />{" "}
-                  {contactData.phone}
-                </a>
-              </li>
-              <li className="my-2">
-                <a target="_blank" href="#" className="flex items-center">
-                  <img
-                    className="w-[20px] mr-2"
-                    src={LetterIcon}
-                    alt="Letter Icon"
-                    placeholder="blur"
-                  />
-                  {contactData.email}
-                </a>
-              </li>
-            </ul>
-          </div>
+          <span className="block my-5 text-[#3e3e3e] font-bold text-[18px]">
+            Let's Find A Solution To Your Problem
+          </span>
+          <span className="block my-5 text-[#3e3e3e] text-[16px]">
+            {contactData.address}
+          </span>
         </div>
-      </footer>
-    </>
+        <div className="md:w-[33%] md:pl-20">
+          <span className="font-bold">Navigation</span>
+          <ul className="md:pt-10 text-[#3e3e3e]">
+            <li className="my-2">
+              <a href="">About Us</a>
+            </li>
+            <li className="my-2">
+              <a href="">Wool For Company</a>
+            </li>
+            <li className="my-2">
+              <a href="">Join as a Coach</a>
+            </li>
+          </ul>
+        </div>
+        <div className="md:w-[33%] md:pl-20">
+          <span className="font-bold">Business & Cooperation</span>
+          <ul className="md:pt-10 text-[#3e3e3e]">
+            <li className="my-2">
+              <a
+                target="_blank"
+                href={`https://wa.me/${process.env.WA_PHONE_NUMBER}`}
+                className="flex items-center"
+              >
+                <img
+                  className="w-[20px] mr-2"
+                  src={PhoneIcon}
+                  alt="Phone Icon"
+                  placeholder="blur"
+                />{" "}
+                {contactData.phone}
+              </a>
+            </li>
+            <li className="my-2">
+              <a target="_blank" href="#" className="flex items-center">
+                <img
+                  className="w-[20px] mr-2"
+                  src={LetterIcon}
+                  alt="Letter Icon"
+                  placeholder="blur"
+                />
+                {contactData.email}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex items-center justify-center pt-4 pb-4 border-t-2 font-[14px] border-[#b2b2b2]">
+        <h4>&#169; 2023 Healing, All Right Reserved</h4>
+      </div>
+    </footer>
   );
 }
